ce303a92dbaabc2bb061e6703f9bac02690e4b11
icecreamsoldier <mariana.9000@hotmail.com>
2018-11-15 10:12:57 -0600

    Test added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const generatePassword = require('password-generator');
 
 const app = express();
 
@@ -13,5 +12,3 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
